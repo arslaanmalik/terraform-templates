07b77473174989279025cb3b504dfb5548a22670
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2025-01-19 13:47:38 +0300

    same var file

--- variables.tf
+++ variables.tf
@@ -15,35 +15,43 @@ variable "zone" {
 variable "vm-name" {
   description = "The name of the VM"
   type        = string
+  default     = "test-vm"
 }
 
 variable "machine_type" {
   description = "This is the machine type"
   type        = string
+  default     = "e2-micro"
 }
 
 variable "device_name" {
-  type = string
+  type    = string
+  default = "terraform-attached-test"
 }
 
 variable "image" {
-  type = string
+  type    = string
+  default = "projects/debian-cloud/global/images/debian-12-bookworm-v20250113"
 }
 
 variable "size" {
-  type = string
+  type    = string
+  default = "20"
 }
 variable "type" {
   description = "This is the Disk Type SSD or Standard"
   type        = string
+  default     = "pd-standard"
 }
 
 variable "disk_label" {
-  type = string
+  type    = string
+  default = "test-app-terraform-disk"
 }
 
 variable "can_ip_forward" {
-  type = string
+  type    = string
+  default = "false"
 }
 
 variable "assign_external_ip" {
@@ -61,5 +69,6 @@ variable "network_tier" {
 # }
 
 variable "deletion_protection" {
-  type = string
+  type    = string
+  default = "false"
 }
